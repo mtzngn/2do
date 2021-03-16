fa411b36e2c6db7460d6d898651794d2b89df49b
Taha <mt.zngn@gmail>
2021-03-16 15:42:28 +0000

    functionality for update taskt state added

--- src/componenets/Task.jsx
+++ src/componenets/Task.jsx
@@ -1,13 +1,39 @@
 import react from"react";
+import styled from"styled-components";
+
+const StyledTask = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items:center;
+    height: 12vh;
+    
+
+
+.check{
+    margin-left: 5px;
+    width: 15%;
+}
+.task{
+    font-size: 0.8rem;
+}
+.time{
+    width: 50%;
+}
+`
 
 const Task = () => {
 return(
-    <>
-    <div>Tasks</div>
-    <div>Tasks</div>
-    <div>Tasks</div>
-    <div>Tasks</div>
-    </>
+    <StyledTask>
+            <div className="check">
+                <input type="radio"></input>
+            </div>
+            <div className="task">
+                <p>Shopping and cooking and cleaning and more and more</p>
+            </div>
+            <div className="time">
+                <p>2:00PM</p>
+            </div>
+    </StyledTask>
 )
 }
 
